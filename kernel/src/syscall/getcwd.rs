bf2ca2714f6ba2a5c47f93b00e611ef64e580ab6
Wang Taojie <12210519@mail.sustech.edu.cn>
2024-10-10 21:09:41 +0800

    Implement getcwd()

--- kernel/src/syscall/getcwd.rs
+++ kernel/src/syscall/getcwd.rs
@@ -1,12 +1,20 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use super::SyscallReturn;
-use crate::{fs::fs_resolver::FsPath, prelude::*};
+use crate::{
+    fs::fs_resolver::{FsPath, AT_FDCWD},
+    prelude::*,
+};
 
 pub fn sys_getcwd(buf: Vaddr, len: usize, ctx: &Context) -> Result<SyscallReturn> {
     let current = ctx.process;
-    let dirent = current.fs().read().lookup(&FsPath::new(-100, "").unwrap()).unwrap();
+    let dirent = current
+        .fs()
+        .read()
+        .lookup(&FsPath::new(AT_FDCWD, "").unwrap())
+        .unwrap();
     let name = dirent.abs_path();
+
     debug!("getcwd: {:?}", name);
 
     let cwd = CString::new(name)?;
@@ -14,6 +22,6 @@ pub fn sys_getcwd(buf: Vaddr, len: usize, ctx: &Context) -> Result<SyscallReturn
     let write_len = len.min(bytes.len());
     ctx.get_user_space()
         .write_bytes(buf, &mut VmReader::from(&bytes[..write_len]))?;
-    
+
     Ok(SyscallReturn::Return(write_len as _))
 }
