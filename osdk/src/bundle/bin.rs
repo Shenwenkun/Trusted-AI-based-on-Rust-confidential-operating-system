asterinas/asterinas

Enhance OSDK performance by using size and modified time instead of sha256sum

osdk/src/bundle/bin.rs
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use std::path::{Path, PathBuf};
+use std::{
+    os::unix::fs::MetadataExt,
+    path::{Path, PathBuf},
+    time::SystemTime,
+};
 
 use super::file::BundleFile;
 use crate::{arch::Arch, util::fast_copy};
@@ -11,7 +15,8 @@ pub struct AsterBin {
     arch: Arch,
     typ: AsterBinType,
     version: String,
-    sha256sum: String,
+    modified_time: SystemTime,
+    size: u64,
     stripped: bool,
 }
 
@@ -41,8 +46,12 @@ impl BundleFile for AsterBin {
         &self.path
     }
 
-    fn sha256sum(&self) -> &String {
-        &self.sha256sum
+    fn modified_time(&self) -> &SystemTime {
+        &self.modified_time
+    }
+
+    fn size(&self) -> &u64 {
+        &self.size
     }
 }
 
@@ -59,11 +68,13 @@ impl AsterBin {
             arch,
             typ,
             version,
-            sha256sum: String::new(),
+            modified_time: SystemTime::UNIX_EPOCH,
+            size: 0,
             stripped,
         };
         Self {
-            sha256sum: created.calculate_sha256sum(),
+            modified_time: created.get_modified_time(),
+            size: created.get_size(),
             ..created
         }
     }
@@ -84,13 +95,15 @@ impl AsterBin {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let file_name = self.path.file_name().unwrap();
         let copied_path = base.as_ref().join(file_name);
-        fast_copy(&self.path, copied_path).unwrap();
+        fast_copy(&self.path, &copied_path).unwrap();
+        let copied_metadata = copied_path.metadata().unwrap();
         Self {
             path: PathBuf::from(file_name),
             arch: self.arch,
             typ: self.typ,
             version: self.version,
-            sha256sum: self.sha256sum,
+            modified_time: copied_metadata.modified().unwrap(),
+            size: copied_metadata.size(),
             stripped: self.stripped,
         }
     }

osdk/src/bundle/file.rs
@@ -1,36 +1,39 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use std::{
-    fs, io,
+    os::unix::fs::MetadataExt,
     path::{Path, PathBuf},
+    time::SystemTime,
 };
-
-use sha2::{Digest, Sha256};
 
 use crate::util::fast_copy;
 
-/// A trait for files in a bundle. The file in a bundle should have it's digest and be validatable.
+/// A trait for files in a bundle. The file in a bundle should have its modified time and be validatable.
 pub trait BundleFile {
     fn path(&self) -> &PathBuf;
 
-    fn sha256sum(&self) -> &String;
+    fn modified_time(&self) -> &SystemTime;
 
-    fn calculate_sha256sum(&self) -> String {
-        let mut file = fs::File::open(self.path()).unwrap();
-        let mut hasher = Sha256::new();
-        let _n = io::copy(&mut file, &mut hasher).unwrap();
-        format!("{:x}", hasher.finalize())
+    fn size(&self) -> &u64;
+
+    fn get_modified_time(&self) -> SystemTime {
+        self.path().metadata().unwrap().modified().unwrap()
+    }
+
+    fn get_size(&self) -> u64 {
+        self.path().metadata().unwrap().size()
     }
 
     fn validate(&self) -> bool {
-        self.sha256sum() == &self.calculate_sha256sum()
+        self.size() == &self.get_size() && self.modified_time() >= &self.get_modified_time()
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Initramfs {
     path: PathBuf,
-    sha256sum: String,
+    modified_time: SystemTime,
+    size: u64,
 }
 
 impl BundleFile for Initramfs {
@@ -38,8 +41,12 @@ impl BundleFile for Initramfs {
         &self.path
     }
 
-    fn sha256sum(&self) -> &String {
-        &self.sha256sum
+    fn modified_time(&self) -> &SystemTime {
+        &self.modified_time
+    }
+
+    fn size(&self) -> &u64 {
+        &self.size
     }
 }
 
@@ -47,10 +54,12 @@ impl Initramfs {
     pub fn new(path: impl AsRef<Path>) -> Self {
         let created = Self {
             path: path.as_ref().to_path_buf(),
-            sha256sum: String::new(),
+            modified_time: SystemTime::UNIX_EPOCH,
+            size: 0,
         };
         Self {
-            sha256sum: created.calculate_sha256sum(),
+            modified_time: created.get_modified_time(),
+            size: created.get_size(),
             ..created
         }
     }
@@ -59,9 +68,10 @@ impl Initramfs {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let name = self.path.file_name().unwrap();
         let dest = base.as_ref().join(name);
-        fast_copy(&self.path, dest).unwrap();
+        fast_copy(&self.path, &dest).unwrap();
         Self {
             path: PathBuf::from(name),
+            modified_time: dest.metadata().unwrap().modified().unwrap(),
             ..self
         }
     }

osdk/src/bundle/vm_image.rs
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use std::path::{Path, PathBuf};
+use std::{
+    os::unix::fs::MetadataExt,
+    path::{Path, PathBuf},
+    time::SystemTime,
+};
 
 use crate::util::fast_copy;
 
@@ -11,7 +15,8 @@ pub struct AsterVmImage {
     path: PathBuf,
     typ: AsterVmImageType,
     aster_version: String,
-    sha256sum: String,
+    modified_time: SystemTime,
+    size: u64,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -35,8 +40,12 @@ impl BundleFile for AsterVmImage {
         &self.path
     }
 
-    fn sha256sum(&self) -> &String {
-        &self.sha256sum
+    fn modified_time(&self) -> &SystemTime {
+        &self.modified_time
+    }
+
+    fn size(&self) -> &u64 {
+        &self.size
     }
 }
 
@@ -46,10 +55,12 @@ impl AsterVmImage {
             path: path.as_ref().to_path_buf(),
             typ,
             aster_version,
-            sha256sum: String::new(),
+            modified_time: SystemTime::UNIX_EPOCH,
+            size: 0,
         };
         Self {
-            sha256sum: created.calculate_sha256sum(),
+            modified_time: created.get_modified_time(),
+            size: created.get_size(),
             ..created
         }
     }
@@ -62,12 +73,14 @@ impl AsterVmImage {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let file_name = self.path.file_name().unwrap();
         let copied_path = base.as_ref().join(file_name);
-        fast_copy(&self.path, copied_path).unwrap();
+        fast_copy(&self.path, &copied_path).unwrap();
+        let copied_metadata = copied_path.metadata().unwrap();
         Self {
             path: PathBuf::from(file_name),
             typ: self.typ,
             aster_version: self.aster_version,
-            sha256sum: self.sha256sum,
+            modified_time: copied_metadata.modified().unwrap(),
+            size: copied_metadata.size(),
         }
     }
 

Shenwenkun/Trusted-AI-based-on-Rust-confidential-operating-system

osdk/src/bundle/bin.rs
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use std::path::{Path, PathBuf};
+use std::{
+    os::unix::fs::MetadataExt,
+    path::{Path, PathBuf},
+    time::SystemTime,
+};
 
 use super::file::BundleFile;
 use crate::{arch::Arch, util::hard_link_or_copy};
@@ -11,7 +15,8 @@ pub struct AsterBin {
     arch: Arch,
     typ: AsterBinType,
     version: String,
-    sha256sum: String,
+    modified_time: SystemTime,
+    size: u64,
     stripped: bool,
 }
 
@@ -41,8 +46,12 @@ impl BundleFile for AsterBin {
         &self.path
     }
 
-    fn sha256sum(&self) -> &String {
-        &self.sha256sum
+    fn modified_time(&self) -> &SystemTime {
+        &self.modified_time
+    }
+
+    fn size(&self) -> &u64 {
+        &self.size
     }
 }
 
@@ -59,11 +68,13 @@ impl AsterBin {
             arch,
             typ,
             version,
-            sha256sum: String::new(),
+            modified_time: SystemTime::UNIX_EPOCH,
+            size: 0,
             stripped,
         };
         Self {
-            sha256sum: created.calculate_sha256sum(),
+            modified_time: created.get_modified_time(),
+            size: created.get_size(),
             ..created
         }
     }
@@ -84,13 +95,15 @@ impl AsterBin {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let file_name = self.path.file_name().unwrap();
         let copied_path = base.as_ref().join(file_name);
-        hard_link_or_copy(&self.path, copied_path).unwrap();
+        hard_link_or_copy(&self.path, &copied_path).unwrap();
+        let copied_metadata = copied_path.metadata().unwrap();
         Self {
             path: PathBuf::from(file_name),
             arch: self.arch,
             typ: self.typ,
             version: self.version,
-            sha256sum: self.sha256sum,
+            modified_time: copied_metadata.modified().unwrap(),
+            size: copied_metadata.size(),
             stripped: self.stripped,
         }
     }

osdk/src/bundle/file.rs
@@ -1,36 +1,39 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use std::{
-    fs, io,
+    os::unix::fs::MetadataExt,
     path::{Path, PathBuf},
+    time::SystemTime,
 };
-
-use sha2::{Digest, Sha256};
 
 use crate::util::hard_link_or_copy;
 
-/// A trait for files in a bundle. The file in a bundle should have it's digest and be validatable.
+/// A trait for files in a bundle. The file in a bundle should have its modified time and be validatable.
 pub trait BundleFile {
     fn path(&self) -> &PathBuf;
 
-    fn sha256sum(&self) -> &String;
+    fn modified_time(&self) -> &SystemTime;
 
-    fn calculate_sha256sum(&self) -> String {
-        let mut file = fs::File::open(self.path()).unwrap();
-        let mut hasher = Sha256::new();
-        let _n = io::copy(&mut file, &mut hasher).unwrap();
-        format!("{:x}", hasher.finalize())
+    fn size(&self) -> &u64;
+
+    fn get_modified_time(&self) -> SystemTime {
+        self.path().metadata().unwrap().modified().unwrap()
+    }
+
+    fn get_size(&self) -> u64 {
+        self.path().metadata().unwrap().size()
     }
 
     fn validate(&self) -> bool {
-        self.sha256sum() == &self.calculate_sha256sum()
+        self.size() == &self.get_size() && self.modified_time() >= &self.get_modified_time()
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Initramfs {
     path: PathBuf,
-    sha256sum: String,
+    modified_time: SystemTime,
+    size: u64,
 }
 
 impl BundleFile for Initramfs {
@@ -38,8 +41,12 @@ impl BundleFile for Initramfs {
         &self.path
     }
 
-    fn sha256sum(&self) -> &String {
-        &self.sha256sum
+    fn modified_time(&self) -> &SystemTime {
+        &self.modified_time
+    }
+
+    fn size(&self) -> &u64 {
+        &self.size
     }
 }
 
@@ -47,10 +54,12 @@ impl Initramfs {
     pub fn new(path: impl AsRef<Path>) -> Self {
         let created = Self {
             path: path.as_ref().to_path_buf(),
-            sha256sum: String::new(),
+            modified_time: SystemTime::UNIX_EPOCH,
+            size: 0,
         };
         Self {
-            sha256sum: created.calculate_sha256sum(),
+            modified_time: created.get_modified_time(),
+            size: created.get_size(),
             ..created
         }
     }
@@ -59,9 +68,10 @@ impl Initramfs {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let name = self.path.file_name().unwrap();
         let dest = base.as_ref().join(name);
-        hard_link_or_copy(&self.path, dest).unwrap();
+        hard_link_or_copy(&self.path, &dest).unwrap();
         Self {
             path: PathBuf::from(name),
+            modified_time: dest.metadata().unwrap().modified().unwrap(),
             ..self
         }
     }

osdk/src/bundle/vm_image.rs
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MPL-2.0
 
-use std::path::{Path, PathBuf};
+use std::{
+    os::unix::fs::MetadataExt,
+    path::{Path, PathBuf},
+    time::SystemTime,
+};
 
 use crate::util::hard_link_or_copy;
 
@@ -11,7 +15,8 @@ pub struct AsterVmImage {
     path: PathBuf,
     typ: AsterVmImageType,
     aster_version: String,
-    sha256sum: String,
+    modified_time: SystemTime,
+    size: u64,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -35,8 +40,12 @@ impl BundleFile for AsterVmImage {
         &self.path
     }
 
-    fn sha256sum(&self) -> &String {
-        &self.sha256sum
+    fn modified_time(&self) -> &SystemTime {
+        &self.modified_time
+    }
+
+    fn size(&self) -> &u64 {
+        &self.size
     }
 }
 
@@ -46,10 +55,12 @@ impl AsterVmImage {
             path: path.as_ref().to_path_buf(),
             typ,
             aster_version,
-            sha256sum: String::new(),
+            modified_time: SystemTime::UNIX_EPOCH,
+            size: 0,
         };
         Self {
-            sha256sum: created.calculate_sha256sum(),
+            modified_time: created.get_modified_time(),
+            size: created.get_size(),
             ..created
         }
     }
@@ -62,12 +73,14 @@ impl AsterVmImage {
     pub fn copy_to(self, base: impl AsRef<Path>) -> Self {
         let file_name = self.path.file_name().unwrap();
         let copied_path = base.as_ref().join(file_name);
-        hard_link_or_copy(&self.path, copied_path).unwrap();
+        hard_link_or_copy(&self.path, &copied_path).unwrap();
+        let copied_metadata = copied_path.metadata().unwrap();
         Self {
             path: PathBuf::from(file_name),
             typ: self.typ,
             aster_version: self.aster_version,
-            sha256sum: self.sha256sum,
+            modified_time: copied_metadata.modified().unwrap(),
+            size: copied_metadata.size(),
         }
     }
 